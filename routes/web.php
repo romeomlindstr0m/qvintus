82873616f2f6fc35b476a7c0c9886a8ea21086f9
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2024-12-12 13:18:17 +0200

    feat: define routes for login and signup GET/POST actions

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
+
+Route::get('/', function() {
+    return view('index');
+})->name('index')->middleware('auth');
+
+Route::get('/register', [AuthenticationController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthenticationController::class, 'register'])->name('register.process');
+
+Route::get('/login', [AuthenticationController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthenticationController::class, 'login'])->name('login.process');
