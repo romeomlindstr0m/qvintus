2550aa3faa19f83d6d4acd4ed17fcaa919f2278a
romeomlindstr0m <romeo.lindstrom06@outlook.com>
2025-01-31 06:33:28 +0200

    Organize routes into middleware groups

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthenticationController;
+use App\Livewire\UserSearch;
 
-Route::get('/login', [AuthenticationController::class, 'showLoginForm'])->name('login.show');
+use App\Http\Controllers\UserController;
+
+Route::get('/login', [AuthenticationController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthenticationController::class, 'login'])->name('login.process')->middleware('throttle:authentication');
 
 Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout')->middleware('auth');
+
+Route::middleware(['auth', 'can:admin-access'])->group(function () {
+    Route::get('/users', UserSearch::class)->name('users.index');
+    Route::post('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+});
 
 Route::redirect('/', '/home');
 Route::get('/home', function() {
